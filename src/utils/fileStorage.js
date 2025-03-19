a8057cf15783e029332ca237d708531acdee6849
Антон Архипов <i.am@mindes.ru>
2025-03-20 00:48:26 +0300

    [*] minor change

--- src/utils/fileStorage.js
+++ src/utils/fileStorage.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../uploads');
+const uploadDir = path.join(__dirname, '../../uploads');
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
